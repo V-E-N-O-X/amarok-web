863db39057ebf59e10d699ba6532319363b77365
Angela <104827062+angela-ser@users.noreply.github.com>
2022-12-05 18:04:28 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,11 @@
-let cluster = require('cluster')
-let path = require('path')
-let fs = require('fs')
-const Readline = require('readline')
+const express = require('express')
+const PORT = process.env.PORT || 3030;
+var app = express();
 
-var isRunning = false
-/**
- * Start a js file
- * @param {String} file `path/to/file`
- */
-function start(file) {
-  if (isRunning) return
-  isRunning = true
-  let args = [path.join(__dirname, file), ...process.argv.slice(2)]
+var main = require('./index2');
 
-  cluster.setupMaster({
-    exec: path.join(__dirname, file),
-    args: args.slice(1),
-  })
-  let p = cluster.fork()
-  p.on('message', data => {
-    console.log('[RECEIVED]', data)
-    switch (data) {
-      case 'reset':
-        p.kill()
-        isRunning = false
-        start.apply(this, arguments)
-        break
-      case 'uptime':
-        p.send(process.uptime())
-        break
-    }
-  })
-  p.on('exit', code => {
-    isRunning = false
-    console.error('Exited with code:', code)
-    if (code === 0) return
-    fs.watchFile(args[0], () => {
-      fs.unwatchFile(args[0])
-      start(file)
-    })
-  })
+app.use('/', index2);
 
-}
-
-
-start('main.js')
+app.listen(PORT, () => {
+    console.log(`Server Run on port ${PORT}`)
+});
